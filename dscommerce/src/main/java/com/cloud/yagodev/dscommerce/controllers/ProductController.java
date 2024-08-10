a1620dbda86da247dc1ba964063cffb016d6421f
Yago Moreira <yagomoreira_dev@yahoo.com>
2024-08-10 20:34:03 -0300

    Add busca pagina de produtos | Pageable - Page

--- dscommerce/src/main/java/com/cloud/yagodev/dscommerce/controllers/ProductController.java
+++ dscommerce/src/main/java/com/cloud/yagodev/dscommerce/controllers/ProductController.java
@@ -2,10 +2,14 @@ package com.cloud.yagodev.dscommerce.controllers;
 
 import com.cloud.yagodev.dscommerce.domain.dtos.ProductDTO;
 import com.cloud.yagodev.dscommerce.services.ProductService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping
@@ -16,8 +20,14 @@ public class ProductController {
     public ProductController(ProductService productService) {
         this.productService = productService;
     }
+
     @GetMapping("/products/{id}")
     public ProductDTO findById(@PathVariable Long id) {
         return productService.findById(id);
+    }
+
+    @GetMapping("/products")
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        return productService.findAll(pageable);
     }
 }
--- dscommerce/src/main/java/com/cloud/yagodev/dscommerce/services/ProductService.java
+++ dscommerce/src/main/java/com/cloud/yagodev/dscommerce/services/ProductService.java
@@ -3,9 +3,12 @@ package com.cloud.yagodev.dscommerce.services;
 import com.cloud.yagodev.dscommerce.domain.dtos.ProductDTO;
 import com.cloud.yagodev.dscommerce.domain.entities.Product;
 import com.cloud.yagodev.dscommerce.repositories.ProductRepository;
-import org.springframework.beans.BeanUtils;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class ProductService {
@@ -20,5 +23,10 @@ public class ProductService {
     public ProductDTO findById(Long id) {
         Product product = productRepository.findById(id).get();
         return new ProductDTO(product);
+    }
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> products = productRepository.findAll(pageable);
+        return products.map(x -> new ProductDTO(x));
     }
 }
