81f3ff258e0ed7cf67ba19daf2137df8f8ad5967
Yago Moreira <yagomoreira_dev@yahoo.com>
2024-08-11 20:12:21 -0300

    Add Delete Product

--- dscommerce/src/main/java/com/cloud/yagodev/dscommerce/controllers/ProductController.java
+++ dscommerce/src/main/java/com/cloud/yagodev/dscommerce/controllers/ProductController.java
@@ -43,4 +43,10 @@ public class ProductController {
         productDTO = productService.update(id, productDTO);
         return ResponseEntity.ok(productDTO);
     }
+
+    @DeleteMapping(value = "/products/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        productService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
